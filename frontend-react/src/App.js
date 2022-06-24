78a5e747f1fc3b52f500a9eebb605078f929b2a5
Nadim <nahlenadim@gmail.com>
2022-06-24 22:57:26 +0300

    Frontend-React Create- 404 Page

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,6 +3,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Contact from './pages/Contact';
 import Layout from './components/Layout';
+import Missing from './pages/Missing';
 
 function App() {
   return (
@@ -10,9 +11,15 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Layout />}>
+          {/*PUBLIC ROUTES*/}
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
+
+          {/*PROTECTED ROUTES*/}
           <Route path="/contact" element={<Contact />}></Route>
+
+          {/*404 ROUTE*/}
+          <Route path="/error" element={<Missing />}></Route>
         </Route>
         
       </Routes>
