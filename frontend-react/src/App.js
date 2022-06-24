26f2d881c9ddf46befd0308c3c56bdf7ea0f7819
Nadim <nahlenadim@gmail.com>
2022-06-24 22:09:03 +0300

    Frontend-React Link- Register API

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,11 +5,12 @@ import Login from './pages/Login';
 
 function App() {
   return (
+    
     <BrowserRouter>
       <Navbar />
       <Routes>
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
