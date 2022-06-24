3259018d4934c98380a78b3993fca235c8deaf1d
Nadim <nahlenadim@gmail.com>
2022-06-24 17:22:30 +0300

    Frontend-React Create- Navbar JSX

---
+++ frontend-react/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  // To get current path
+  const location = useLocation();
+  // Iterate over nav items to add into UI
+  const navItems = [
+    { link: "", name: "Login" },
+    { link: "", name: "Signup" },
+  ];
+  return (
+    <nav className="top-bar">
+      <ul className="nav-items">
+        {navItems.map((navItem, index) => {
+          return (
+            <NavLink key={index} to={navItem.link}>
+              <li>{navItem.name}</li>
+            </NavLink>
+          );
+        })}
+        {location.pathname.includes("auth") && (
+          <NavLink to="">
+            <li>Logout</li>
+          </NavLink>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
