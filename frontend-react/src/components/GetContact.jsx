14718f9a07afad230fd62a553bf1534846a9e7fa
Nadim <nahlenadim@gmail.com>
2022-06-25 11:33:45 +0300

    Front-React Link- Get Contacts API

--- frontend-react/src/components/GetContact.jsx
+++ frontend-react/src/components/GetContact.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from '../api/axios';
+
+const CONTACT_URL ='/api/v1/auth/contacts';
 
 const GetContact = () => {
-  return (
-    <div>GetContact</div>
-  )
+
+    const[contacts, setContacts] = useState([null]);
+    
+    useEffect(()=>{
+
+        var jwt = localStorage.getItem('token');
+
+        axios.get(CONTACT_URL,{
+            headers: {'Authorization': 'Bearer '+jwt}
+        })
+
+        .then( (result) => {
+            //console.table(result.data);
+            setContacts(result.data);
+        });
+
+    }, []);
+
+    return (
+        <div>
+            {contacts ? <div>Contacts</div> : <div>No Contacts</div>}
+        </div>
+
+    )
 }
 
 export default GetContact
