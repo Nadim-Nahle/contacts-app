2390f4f8496774db88aa337c16c1a8f311410a78
Nadim <nahlenadim@gmail.com>
2022-06-26 18:18:42 +0300

    Frontend-React Create- popup component

--- frontend-react/src/components/Leaflet.jsx
+++ frontend-react/src/components/Leaflet.jsx
@@ -5,14 +5,16 @@ import { MapContainer, TileLayer } from "react-leaflet";
 const Leaflet = () => {
   return (
     <div className="map-center">
+        <h1>Click the pin and drag it </h1>
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <DraggableMarker />
+        <DraggableMarker /> 
       </MapContainer>
+      
     </div>
   );
 };
